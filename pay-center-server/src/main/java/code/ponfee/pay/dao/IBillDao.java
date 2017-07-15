33328d57008fa52d2a0fdc4d168980e032213132
china-fupengfei <fupengfei.china@gmail.com>
2017-07-15 13:57:26 +0800

    优化page

--- pay-center-server/src/main/java/code/ponfee/pay/dao/IBillDao.java
+++ pay-center-server/src/main/java/code/ponfee/pay/dao/IBillDao.java
@@ -3,7 +3,7 @@ package code.ponfee.pay.dao;
 import java.util.List;
 import java.util.Map;
 
-import code.ponfee.commons.model.Pagination;
+import code.ponfee.commons.model.Page;
 import code.ponfee.pay.model.OrderBill;
 import code.ponfee.pay.model.Refund;
 
@@ -13,7 +13,7 @@ public interface IBillDao {
     
     List<OrderBill> listRefundBill(String payId);
     
-    Pagination<Refund> queryRefundsForPage(Map<String, ?> params);
+    Page<Refund> queryRefundsForPage(Map<String, ?> params);
     
     /*用于手动退测试支付时的钱款*/
     //List<Payment> listPayment4testPay(String beginDate, String endDate);
